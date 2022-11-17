a8d200c7d33a7ffd43f07f1ac4aa2fd80751b573
GitHub Action <action@github.com>
2022-11-17 09:37:30 +0000

    Template cleanup

--- src/main/kotlin/com/github/berlaq/plugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/berlaq/plugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.berlaq.plugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.berlaq.plugin.MyBundle
 
 class MyApplicationService {
 
